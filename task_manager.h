61d3e1463baaff7614b333b85fb21042996d8852
Austinfer <ozzyandrew@hotmail.com>
2025-03-26 17:01:59 -0400

    1. add a new task function added 2. delete an existing task function added 3. update an existing task added 4. base menu/UI system 5. added base .c and .h files

---
+++ task_manager.h
@@ -0,0 +1,12 @@
+#ifndef TASK_MANAGER_H
+#define TASK_MANAGER_H
+
+#include "task_list.h"
+
+// Function to display the menu
+void displayMenu();
+
+// Function to handle user choices
+void handleUserChoice();
+
+#endif
