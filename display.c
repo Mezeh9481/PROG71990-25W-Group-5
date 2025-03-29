757d67a27b129278700ae4e183720eb182fa8a81
DisguisedMax <113708838+DisguisedMax@users.noreply.github.com>
2025-03-29 12:25:13 -0400

    Display.h and Display.c files created

---
+++ display.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "display.h"
+
+void displayItem() {
+    printf("Displaying a single item...\n");
+}
+
+void displayRange() {
+    printf("Displaying a range of items...\n");
+}
+
+void displayAll() {
+    printf("Displaying all items...\n");
+}
---
+++ display.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+// Function to display a single item
+void displayItem();
+
+// Function to display a range of items
+void displayRange();
+
+// Function to display all items
+void displayAll();
+
+#endif 
