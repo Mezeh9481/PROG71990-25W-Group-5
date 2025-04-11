76f85522245a7094038529dc3b2efc739e4ffc69
Mezeh9481 <Mezeh9481@conestogac.on.ca>
2025-04-11 13:38:11 -0400

    Modification to code to accomadate merge to master branch

--- task_manager.c
+++ task_manager.c
@@ -7,11 +7,13 @@ void displayMenu() {
     printf("1. Add New Task\n");
     printf("2. Delete Task\n");
     printf("3. Update Task\n");
-    printf("4. View Tasks\n");
-    printf("5. Search Task\n");
-    printf("6. Save Tasks\n");
-    printf("7. Mark Task as complete\n");
-    printf("8. Exit\n");
+    printf("4. Display Single Task\n");
+    printf("5. Display Task Range\n");
+    printf("6. Display All Tasks\n");
+    printf("7. Search Task\n");
+    printf("8. Save Tasks\n");
+    printf("9. Mark Task as complete\n");
+    printf("10. Exit\n");
     printf("======================\n");
     printf("Enter your choice: ");
 }
@@ -37,19 +39,47 @@ void handleUserChoice() {
         case 3:
             updateTask(&taskList);
             break;
-        case 4:
-            viewTasks(&taskList);
+        case 4: { // Display single task
+            int id;
+            printf("Enter Task ID to display: ");
+            scanf("%d", &id);
+            getchar();
+
+            Task* target = NULL;
+            for (int i = 0; i < taskList.count; i++) {
+                if (taskList.tasks[i].id == id) {
+                    target = &taskList.tasks[i];
+                    break;
+                }
+            }
+            displayItem(target);
             break;
-        case 5:
-            searchTask(&taskList);
+        }
+        case 5: { // Display range
+            int start, end;
+            printf("Enter start task number: ");
+            scanf("%d", &start);
+            printf("Enter end task number: ");
+            scanf("%d", &end);
+            getchar();
+
+            // Adjust to 0-based internally
+            displayRange(&taskList, start - 1, end - 1);
             break;
-        case 6:
-            saveTasksToFile(&taskList, "tasks.txt");
+        }
+        case 6: // Display all
+            displayAll(&taskList);
             break;
         case 7:
-            markTaskAsComplete(&taskList);
+            searchTask(&taskList);
             break;
         case 8:
+            saveTasksToFile(&taskList, "tasks.txt");
+            break;
+        case 9:
+            markTaskAsComplete(&taskList);
+            break;
+        case 10:
             printf("Exiting Task Manager. Goodbye!\n");
             break;
         default:
